0576937e46f48a128ade93c82afc923914522e6a
Till <tillbungert@gmail.com>
2016-03-06 11:37:07 +0100

    222: create table

--- init.py
+++ init.py
@@ -9,7 +9,11 @@ import warnings
 def init():
     warnings.filterwarnings('ignore')
 
-def valerr(id, val, err, unit=""):
+def valerr(id, val, err, r=-1, unit=""):
+    if r != -1:
+        val = float64(val).round(r)
+        err = float64(err).round(r)
+
     if unit is "":
         display(Latex(r"${0} = {1} \pm {2}$".format(id, val, err)))
     else:
