9214d81013f3cf617641eedd906c1b5e5c21385b
Till Bungert <Till Bungert>
2016-04-27 19:21:13 +0200

    252: finish Ag

--- init.py
+++ init.py
@@ -3,21 +3,32 @@ from matplotlib.pyplot import *
 import pandas as pd
 import scipy as sp
 import sympy as sm
-from IPython.display import display, Latex, Math
+from IPython.display import display, Latex, Math, Markdown, HTML
 import warnings
+
+version = "2.0.0"
 
 def init():
     warnings.filterwarnings('ignore')
 
-def valerr(id, val, err, r=-1, unit=""):
+def to_valerr(id, val, err, r=-1, unit=""):
     if r != -1:
         val = float64(val).round(r)
         err = float64(err).round(r)
 
     if unit is "":
-        display(Latex(r"${0} = {1} \pm {2}$".format(id, val, err)))
+        return r"${0} = {1} \pm {2}$".format(id, val, err)
     else:
-        display(Latex(r"${0} = {1} \pm {2} \ {3}$".format(id, val, err, unit)))
+        return r"${0} = {1} \pm {2} \ {3}$".format(id, val, err, unit)
+
+def display_valerr(valerr):
+    display(Latex(valerr))
 
 def chi2(data, fdata, err):
     return sum(((data-fdata)/err)**2)
+
+def display_valtable(keyvals):
+    tables = []
+    for key, val in keyvals:
+        tables.append("<tr><td>{0}</td><td>{1}</td></tr>".format(key, val))
+    display(HTML('<table>{}</table>'.format("".join(tables))))
