587f902595a44d7d6ad3a92ac9f36b05b029792b
Till Bungert <Till Bungert>
2016-06-09 22:42:23 +0200

    Add Table class, start 241

--- pap2_2/pap2.py
+++ pap2_2/pap2.py
@@ -6,6 +6,8 @@ from numpy import *
 from scipy.optimize import curve_fit
 from matplotlib.pyplot import *
 from IPython.display import *
+from prettytable import PrettyTable
+from html import escape
 
 import warnings
 
@@ -19,15 +21,36 @@ def chi2(data, fdata, err):
     """
     return sum(((data-fdata)/err)**2)
 
-class Table(object):
-    """
-    HTML Table Class
-    """
-    def __init__(self):
-        pass #TODO: Run real __init__ function with filler values
 
-    #TODO: Implement
-
-def display_table():
+def pformat(data):
     #TODO: Implement
     pass
+
+
+class Table(PrettyTable):
+    """docstring for Table"""
+
+    def __init__(self, title):
+        super(Table, self).__init__()
+        self._title = title
+
+    def get_html_string(self, **kwargs):
+        title = None
+        if "title" in kwargs.keys():
+            title = kwargs["title"]
+            kwargs["title"] = ""
+        if self.title:
+            title = self.title
+            self.title = ""
+        html = super(Table, self).get_html_string(**kwargs).splitlines()
+        if title:
+            html.insert(1, "<h3>{0}</h3>".format(title))
+
+        return "\n".join(html)
+
+
+def display_table(table):
+    if isinstance(table, Table):
+        display(HTML(table.get_html_string()))
+    else:
+        display(table)
